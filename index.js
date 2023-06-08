69a2117d0425c13b8ba2f5ea91fb20d0c7a5bf1d
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-08 18:50:23 -0400

    feat: add code for one game round

--- index.js
+++ index.js
@@ -3,19 +3,76 @@ function getComputerChoice() {
     let choice;
     switch(num) {
         case 0:
-            choice = 'rock';
+            choice = 'Rock';
             break;
         case 1:
-            choice = 'paper';
+            choice = 'Paper';
             break;
         case 2:
-            choice = 'scissor';
+            choice = 'Scissors';
             break;
         default:
-            choice = 'try again';
+            choice = 'Try Again';
             break;
     }
     return choice;
 }
 
-function getPlayerChoice()
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    let res;
+
+    // rock
+    if (/rock/i.test(playerSelection)) {
+        // execute
+        switch (computerSelection) {
+            case 'Rock':
+                res = `It's a tie! You think like a computer.`;
+                break;
+            case 'Paper':
+                res = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+            case 'Scissors':
+                res = `You Win! ${playerSelection} beats ${computerSelection}`;
+                break;
+        }
+
+    } else if (/paper/i.test(playerSelection)) {
+        // execute
+        switch (computerSelection) {
+            case 'Rock':
+                res = `You Win! ${playerSelection} beats ${computerSelection}`;
+                break;
+            case 'Paper':
+                res = `It's a tie! You think like a computer.`;
+                break;
+            case 'Scissors':
+                res = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+        }
+
+    } else if (/scissors/i.test(playerSelection)) {
+        // execute
+        switch (computerSelection) {
+            case 'Rock':
+                res = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+            case 'Paper':
+                res = `You Win! ${playerSelection} beats ${computerSelection}`;
+                break;
+            case 'Scissors':
+                res = `It's a tie! You think like a computer.`;
+                break;
+        }
+
+    } else {
+        res = `Invalid Input! Please enter Rock, Paper or Scissors only.`
+    }
+
+    return res;
+  }
+
+console.log(playRound(playerSelection, computerSelection));
+
