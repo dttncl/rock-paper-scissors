be84e8dff7255a5e8d4b6c20d0e140536715fd6e
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-19 17:26:34 -0400

    add javascript player round

--- index.js
+++ index.js
@@ -1,85 +1,105 @@
+const buttons = document.querySelectorAll('button.human');
+const human = document.querySelector('#human-score');
+const machine = document.querySelector('#machine-score');
+
+const rock = document.querySelector('#machine-rock');
+const paper = document.querySelector('#machine-paper');
+const scissors = document.querySelector('#machine-scissors');
+
+let humanScore = 0;
+let machineScore = 0;
+
+// randomize computer bet
 function getComputerChoice() {
     let num = Math.floor(Math.random()*3);
     let choice;
     switch(num) {
         case 0:
             choice = 'Rock';
+            rock.classList.add('machine');
+            paper.classList.remove('machine');
+            scissors.classList.remove('machine');
             break;
         case 1:
             choice = 'Paper';
+            paper.classList.add('machine');
+            rock.classList.remove('machine');
+            scissors.classList.remove('machine');
             break;
         case 2:
             choice = 'Scissors';
+            scissors.classList.add('machine');
+            paper.classList.remove('machine');
+            rock.classList.remove('machine');
             break;
         default:
             choice = 'Try Again';
             break;
     }
+
     return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    let res;
+    let winner = '';
 
     if (/rock/i.test(playerSelection)) {
-        // execute
         switch (computerSelection) {
             case 'Rock':
-                res = `It's a tie! You think like a computer.`;
+                winner += 'both';
                 break;
             case 'Paper':
-                res = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                winner += 'machine';
                 break;
             case 'Scissors':
-                res = `You Win! ${playerSelection} beats ${computerSelection}`;
+                winner += 'human';
                 break;
         }
 
     } else if (/paper/i.test(playerSelection)) {
-        // execute
         switch (computerSelection) {
             case 'Rock':
-                res = `You Win! ${playerSelection} beats ${computerSelection}`;
+                winner += 'human';
                 break;
             case 'Paper':
-                res = `It's a tie! You think like a computer.`;
+                winner += 'both';
                 break;
             case 'Scissors':
-                res = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                winner += 'machine';
                 break;
         }
 
     } else if (/scissors/i.test(playerSelection)) {
-        // execute
         switch (computerSelection) {
             case 'Rock':
-                res = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                winner += 'machine';
                 break;
             case 'Paper':
-                res = `You Win! ${playerSelection} beats ${computerSelection}`;
+                winner += 'human';
                 break;
             case 'Scissors':
-                res = `It's a tie! You think like a computer.`;
+                winner += 'both';
                 break;
         }
 
-    } else {
-        res = `Invalid Input! Please enter Rock, Paper or Scissors only.`
-    }
-    return res;
+    } 
+
+    return winner;
 }
 
-//console.log(playRound(playerSelection, computerSelection));
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let playerSelection = btn.value;
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
 
-function game() {
+        if (result === 'human') {
+            humanScore++;
+        } else if (result === 'machine') {
+            machineScore++;
+        }
 
-    /*
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt(`Enter your bet (Rock, Paper, Scissors):`);
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    */
-}
-
-game();
+        human.textContent = humanScore;
+        machine.textContent = machineScore;
+    })
+});
