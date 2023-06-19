b715174da8ab0c7355933f54a41432fdf1fb66e7
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-19 16:24:10 -0400

    add style to rps

--- index.js
+++ index.js
@@ -72,11 +72,14 @@ function playRound(playerSelection, computerSelection) {
 //console.log(playRound(playerSelection, computerSelection));
 
 function game() {
+
+    /*
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt(`Enter your bet (Rock, Paper, Scissors):`);
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }
+    */
 }
 
 game();
